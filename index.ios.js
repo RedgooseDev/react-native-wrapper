4d57b49002d7430edbf19af5b1440f0f397bd279
redgoose <scripter@me.com>
2017-07-03 17:15:01 +0900

    데모 조정

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
 
-import Demo from './Demo';
+import Demo from './components';
 
 
 export default class App extends React.Component {
